85fd6093f2dea8238e9abf6aceeebf6924bb4a19
Elayabharathi <elayabharathi.mail@gmail.com>
2026-08-26 16:43:21 +0530

    contact,term,privacy  add  Angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { Privacy } from './pages/privacy/privacy';
+import { Terms } from './pages/terms/terms';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
 
@@ -19,6 +22,20 @@ export const routes: Routes = [
   {
     path: 'register',
     component: Register
+  },
+
+  {
+    path: 'privacy',
+    component: Privacy
+  },
+
+  {
+    path: 'terms',
+    component: Terms
+  },
+  {
+    path: 'contact',
+    component:Contact
   },
 
   {
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
